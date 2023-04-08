41cb06c9d232c4e1d321d1993550c7ed06047b45
ethan-id <ehancock1648@gmail.com>
2023-04-08 17:25:03 -0500

    Browse items are searchable

--- src/Browse.js
+++ src/Browse.js
@@ -8,6 +8,7 @@ function Browse() {
   // view = 3 : confirmation
   const [view, setView] = useState(0);
   const [data, setData] = useState([]); 
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
       fetchData();
@@ -19,15 +20,18 @@ function Browse() {
       ).json();
       setData(response);
   };
-
-  console.log(data);
   
   return (
     view === 0 ? <div className="Browse container">
-      <input class="search" placeholder="Search..."></input>
-      <button onClick={() => {setView(1)}} type="button" class="checkout btn btn-primary">Checkout</button>
-      <div class="productsCont row row-cols-4">
-        {data.map((product) => {
+      <div class="navbar">
+        <input onChange={() => {setFilter(document.getElementById("search").value.toLowerCase())}}id="search" class="search" placeholder="Search..."/>
+        <button onClick={() => {setView(1)}} type="button" class="checkout btn btn-primary">Checkout</button>
+      </div>
+      <div class="productCont">
+        {data.filter((element) => {
+          console.log(filter);
+          return element.title.toLowerCase().includes(filter);
+        }).map((product) => {
           return <Product {...product}/>
         })}
       </div>
--- src/Product.js
+++ src/Product.js
@@ -5,22 +5,22 @@ const Product = (product) => {
     let [quantity, setQuantity] = useState(0);
 
     return (
-        <div class="card shadow-sm m-4">
+        <div class="product card shadow-sm">
             <img src={product.image} class="image" alt={product.title}></img>
             <div class="card-body">
                 <span class="price badge text-bg-primary">{product.price}</span>
                 <h4>{product.title}</h4>
                 <p class="card-text">{product.description}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <button onClick={() => {setQuantity(quantity+1)}} type="button" class="btn btn btn-outline-secondary">+</button>
-                        <button onClick={() => {
-                            if((quantity - 1) >= 0) {
-                                setQuantity(quantity-1)
-                            }
-                        }} type="button" class="btn btn btn-outline-secondary">-</button>
-                        <button type="button" class="btn btn btn-outline-secondary">{quantity}</button>
-                    </div>
+            </div>
+            <div class="card-footer d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                    <button onClick={() => {setQuantity(quantity+1)}} type="button" class="btn btn btn-outline-secondary">+</button>
+                    <button onClick={() => {
+                        if((quantity - 1) >= 0) {
+                            setQuantity(quantity-1)
+                        }
+                    }} type="button" class="btn btn btn-outline-secondary">-</button>
+                    <button type="button" class="btn btn btn-outline-secondary">{quantity}</button>
                 </div>
             </div>
         </div>
